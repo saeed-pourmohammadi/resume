747fe05bd4a49f0b196596d7a5315c39eff3dacf
saeed-pourmohammadi <94374692+saeed-pourmohammadi@users.noreply.github.com>
2024-05-15 15:58:00 +0330

    change production config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   distDir: "docs",
+  assetPrefix: isProd ? "https://saeed-pourmohammadi.github.io/resume" : "/",
 };
 
 export default nextConfig;
