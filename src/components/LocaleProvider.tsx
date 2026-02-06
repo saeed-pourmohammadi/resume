831987ab1e6b3a06e442d9167f292744768c9107
saeed-pourmohammadi <saeed.pourmohammadi1998@gmail.com>
2026-02-06 22:02:32 +0330

    update

--- src/components/LocaleProvider.tsx
+++ src/components/LocaleProvider.tsx
@@ -12,7 +12,7 @@ export default function LocaleProvider({ children }: { children: ReactNode }) {
   const [locale, setLocale] = useState<'en' | 'de'>('de');
 
   return (
-    <div className='bg-gray-50'>
+    <div className='bg-gray-50 header'>
       <div className='max-w-5xl mx-auto flex justify-center p-2 gap-2'>
         <button className={`flex items-center justify-center gap-2 ${locale === "en" ? "bg-blue-600" : "bg-blue-300"} hover:bg-blue-700 text-white font-bold py-2 px-6 rounded disabled:opacity-50 w-44`} onClick={() => setLocale('en')}>EN</button>
         <button className={`flex items-center justify-center gap-2 ${locale === "de" ? "bg-blue-600" : "bg-blue-300"} hover:bg-blue-700 text-white font-bold py-2 px-6 rounded disabled:opacity-50 w-44`} onClick={() => setLocale('de')}>DE</button>
